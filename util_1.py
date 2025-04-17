a78cd6b358febcaafc87fc45feccc987932b8291
chaitanyaa <chaitanyasharma0007@gmail.com>
2025-04-17 13:53:53 +0530

    inital commit

--- util_1.py
+++ util_1.py
@@ -18,7 +18,7 @@ def set_background_image(image_url):
     )
 
 # Your background image URL
-image_url = "https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+image_url = "://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
 
 def fetch_poster(movie_id):
@@ -31,7 +31,7 @@ def fetch_poster(movie_id):
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzJlYjU4ZDIzNjlkMTQzMzhkYmU0NzRlNzhlZDA2ZiIsIm5iZiI6MTc0NDM5ODA4MS4zNzEsInN1YiI6IjY3Zjk2NzAxN2Y3MGFjMWEyMWQ5N2EwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jdJGhmeXR59qbNDJMlGQDRTi7F_t0mxk6PhiMnYDJkg"
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(uhttpsrl, headers=headers)
     response = response.json()
     poster_path = response['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
